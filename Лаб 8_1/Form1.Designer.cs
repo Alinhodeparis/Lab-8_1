57267bb5bfce044515671adb8955fe779e2dc58b
Baly Diabagate <diabagatebaly@gmail.com>
2022-05-01 15:53:43 +0700

    Ajoutez des fichiers projet.

---
+++ Лаб 8_1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Лаб_8_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btAnswer = new System.Windows.Forms.Button();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 59);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btAnswer
+            // 
+            this.btAnswer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAnswer.Location = new System.Drawing.Point(157, 225);
+            this.btAnswer.Name = "btAnswer";
+            this.btAnswer.Size = new System.Drawing.Size(218, 68);
+            this.btAnswer.TabIndex = 1;
+            this.btAnswer.Text = "Answer!";
+            this.btAnswer.UseVisualStyleBackColor = false;
+            this.btAnswer.Click += new System.EventHandler(this.btAnswer_Click);
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.AutoSize = true;
+            this.labelAnswer.Location = new System.Drawing.Point(251, 311);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(0, 13);
+            this.labelAnswer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 376);
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.btAnswer);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btAnswer;
+        private System.Windows.Forms.Label labelAnswer;
+    }
+}
+
---
+++ Лаб 8_1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаб_8_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double probability = 0.5;
+
+        Random rnd = new Random();
+
+        string text;
+
+        private void btAnswer_Click(object sender, EventArgs e)
+        {
+            text = textBox1.Text;
+
+            if (text.Length == 0)
+            {
+                labelAnswer.Text = "Ask a question!";
+                labelAnswer.Location = new Point(110, 95);
+                return;
+            }
+
+            if (rnd.NextDouble() < probability)
+            {
+                labelAnswer.Text = "Yes!";
+                labelAnswer.Location = new Point(250, 150);
+            }
+            else
+            {
+                labelAnswer.Text = " No!";
+                labelAnswer.Location = new Point(250, 150);
+            }
+        }
+    }
+}
